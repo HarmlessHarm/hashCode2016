cbf30e42d793fea505b0ee562ec5d33398b85e55
tintintje <christine.vinck@gmail.com>
2016-02-11 22:17:34 +0100

    commit

--- HashCode2016.py
+++ HashCode2016.py
@@ -8,21 +8,37 @@ ordersInfo = {}
 # warehouses
 nWarehouses = 0
 warehouses = []
+
 coordinates = []
 nItemsOfProduct = []
 
 # Per order
+nOrders = 0
 orderCoords = []
-orderNItems = []
+nOrderItems = []
 itemsOfType = []
 
 def main():
     readFile("./input/mother_of_all_warehouses.in")
 
-    print(parameters)
-    print(productsInfo)
-    print(warehousesInfo)
-    print(ordersInfo)
+    # print(parameters)
+    # print(productsInfo)
+    #
+    # print "items of type", itemsOfType
+    # print "number of items", nOrderItems
+    # print "order coord", orderCoords
+    orderList = []
+    for i, itemList in enumerate(itemsOfType):
+        for item in itemList:
+            x,y = orderCoords[i]
+            newOrder = classes.Order(i, x,y, item)
+            orderList.append(newOrder)
+    print sum(nOrderItems)
+    print len(orderList)
+
+    for i in
+    # print(warehousesInfo)
+    # print(ordersInfo)
 
     # warehouses
     # print(warehouses)
@@ -36,8 +52,9 @@ def main():
 
 def readFile(inputfile):
     # import file
-    global nWarehouses
-    whid = 0
+    global nWarehouses, nOrders
+    whid = 0    # warehouse id
+    oid = 0     # order id
     with open(inputfile, 'r') as rf:
         # read file lines
         for i, line in enumerate(rf):
@@ -60,35 +77,37 @@ def readFile(inputfile):
                 if i % 2 == 0:
                     lineList = line.split()
                     # coords = ()
-                    warehouse = classes.Warehouse(int(lineList[0]), int(lineList[1]))
+                    warehouse = classes.Warehouse(whid, int(lineList[0]), int(lineList[1]))
                     whid += 1
                     warehouses.append(warehouse)
                     # coordinates.append(coords)
                 else:
                     for i, n in enumerate(line.split()):
                         for j in range(int(n)):
-                            newProduct = classes.Product(productInfo['weights'][i], i)
+                            newProduct = classes.Product(productsInfo['weights'][i], i)
                             warehouses[-1].addStock(newProduct)
                     # nItemsOfProduct = [int(n) for n in line.split()]
                 print warehouses[0].x, warehouses[0].y
             # Orders
             if i == (3 + nWarehouses * 2) + 1:
-                ordersInfo["nOrders"] = int(line)
+                nOrders = int(line)
 
             if i > (3 + nWarehouses * 2) + 1 and \
-                i <= ( (3 + nWarehouses * 2) + 1) + ordersInfo["nOrders"] * 2:
+                i <= ( (3 + nWarehouses * 2) + 1) + (nOrders * 3):
                 # get destination coordinate
                 if i % 3 == 0:
+                    itemsOfType.append([int(n) for n in line.split()])
+                # get number of items
+                elif i % 3 == 1:
                     lineList = line.split()
                     coords = (int(lineList[0]), int(lineList[1]))
                     orderCoords.append(coords)
-                # get number of items
-                elif i % 3 == 1:
-                    orderItems.append(int(line))
+                    oid += 1
                 # get number of items per type
                 elif i % 3 == 2:
-                    itemsOfType = [int(n) for n in line.split()]
+                    nOrderItems.append(int(line))
 
+                order = classes.Order(oid, int(lineList[0]), int(lineList[1]), 0)
 
 # def makeWarehouses():
 #     for i in range(warehousesInfo["nWarehouses"]):
