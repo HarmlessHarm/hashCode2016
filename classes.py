8e99a0932b05162505931e62e4934ac091bbe83d
Harm Manders <harmmanders@gmail.com>
2016-02-11 20:37:00 +0100

    added input files and Order class

--- classes.py
+++ classes.py
@@ -20,4 +20,29 @@ class Warehouse(object):
 		self.stock[pType].append(product)
 
 		
+class Order(object):
+	"""docstring for Order"""
+	def __init__(self, destX, destY, pType):
+		# super(Order, self).__init__()
+		self.destX = destX
+		self.destY = destY
+		self.pType = pType
+
+class Drone(object):
+	"""docstring for Drone"""
+	def __init__(self):
+		# super(Drone, self).__init__()
+		# self.arg = arg
+
+	def updatePos(self):
+		self.currentX = self.targetX
+		self.currentY = self.targetY
+
+	def updateTarget(self, targetX, targetY):
+		self.targetX = targetX
+		self.targetY = targetY
+
+	def setTimeToTarget(self, time):
+		self.timeToTarget = time
+		
 		
